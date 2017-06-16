29344dc65aa7d63e93b4c1f17b90cc0cf39557ed
Casper Biemans <casperbiemans@MacBook-Pro-van-Casper.local>
2017-06-16 20:24:12 +0200

    Added Cardview and data model to project.

---
+++ Animated Cards/CardView.swift
@@ -0,0 +1,51 @@
+//
+//  CardView.swift
+//  Animated Cards
+//
+//  Created by Casper Biemans on 16-06-17.
+//  Copyright © 2017 Casper Biemans. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    @IBOutlet var view: UIView!
+    @IBOutlet weak var category: UILabel!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var question: UILabel!
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Load the nib named 'CardView' into memory, finding it in the main bundle.
+        Bundle.main.loadNibNamed("CardView", owner: self, options: nil)
+        
+        // Adding the 'contentView' to self (self represents the instance of a WeatherView which is a 'UIView').
+        addSubview(view)
+        
+        // Style the custom view
+//        view.backgroundColor = UIColor.red
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.lightGray.cgColor
+
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        
+//        self.backgroundColor = UIColor.red
+
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Animated Cards/ViewController.swift
+++ Animated Cards/ViewController.swift
@@ -10,14 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Declare outlets
+    @IBOutlet weak var deck: UIView!
+    
+    // Hold filled cards
+    var cardViews = [CardView]()
+    
+    var playCards:[Card] = [
+        Card(category: "Gear", title: "Ronin M vs. Glidecam", icon: "Icon", question: "Do you prefer a Ronin-M over a Glidecam?"),
+        Card(category: "Music", title: "Flume", icon: "Icon", question: "What do you think of Flume?"),
+        Card(category: "Video", title: "Rory Kramer", icon: "Icon", question: "Do you know Rory Kramer?"),
+        Card(category: "Travel", title: "Casey Neistat", icon: "Icon", question: "Does Casey Neistat live in New York?"),
+        Card(category: "Fashion", title: "Summer '17", icon: "Icon", question: "What would you where this summer?"),
+        Card(category: "Music", title: "Favorite track", icon: "Icon", question: "What is your favorite track off all time?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create deck of cards (20)
+        initDeckOfCards()
+        self.deck.addSubview(cardViews[0])
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // Create deck of cards and fill in the blanks
+    func initDeckOfCards() {
+//        print("Before: \(cardViews.count)")
+        
+        // Create for each entity an UIView Card and add data to it
+        for (index, card) in playCards.enumerated() {
+//            print("Create UIView and filling in the blanks")
+//            print("Item \(index): \(card.category)")
+            
+            // Create CardView()
+            let cardView = CardView()
+            
+            // Fill with data
+            cardView.category.text = playCards[index].category
+            cardView.title.text = playCards[index].title
+//            cardView.icon.image = UIImage(named: playCards[index].icon)
+            cardView.question.text = playCards[index].question
+            
+            
+            // Add view to an array of UIView (CardView)
+            cardViews.append(cardView)
+            
+        }
+//        print("After: \(cardViews.count)")
+
     }
 
 
